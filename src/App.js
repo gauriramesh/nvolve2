cab44516028df6dd53236a3d3b92da049b92ac8d
Karl Shaffer <skmshaffer@users.noreply.github.com>
2019-10-15 10:41:11 -0500

    Prettier Installation and Fixes.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router, 
-  Switch, 
-  Route, 
-} from "react-router-dom"; 
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./Nav";
 import Dashboard from "./Dashboard";
 import Organization from "./Organization";
-import NotFound from "./NotFound"
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <Router>
       <Nav />
       <Switch>
-        <Route path="/organizations/:org" component={Organization}/>
-        <Route exact path="/" component={Dashboard}/>
-        <Route path="/" component={NotFound}/>
+        <Route path="/organizations/:org" component={Organization} />
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/" component={NotFound} />
       </Switch>
     </Router>
   );
--- src/Organization.js
+++ src/Organization.js
@@ -1,40 +1,25 @@
-import React from "react"; 
-import "./Organization.css"; 
+import React from "react";
+import "./Organization.css";
 
 const One = () => {
-    return(
+  return (
     <div>
-        <h1>One!</h1> 
+      <h1>One!</h1>
     </div>
-    );
+  );
 };
 
 const Two = () => {
-    return(
-        <div>
-            <h1>Two!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Two!</h1>
+    </div>
+  );
 };
 
-const Organization = (props) => {
-
-    const { org } = props.match.params;
-    let component; 
-
-    if (org === "test") {
-        component = <One/>
-    } else {
-        component = <Two/>
-    }
-
-return (
-   org === "test" ? <One /> : <Two />
-);
-        <div>
-            { component }
-        </div>
-    );
+const Organization = props => {
+  const { org } = props.match.params;
+  return org === "test" ? <One /> : <Two />;
 };
 
-export default Organization; 
+export default Organization;
