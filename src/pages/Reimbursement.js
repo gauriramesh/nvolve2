ed441be8f5aa5e26f867af016d605554f4a32d8a
AllisonInman <ainman514@hotmail.com>
2019-10-21 20:26:11 -0500

    Add start of validation

--- src/pages/Reimbursement.js
+++ src/pages/Reimbursement.js
@@ -1,54 +1,131 @@
 import React from "react";
-import { Button, Container, Form, FormGroup, FormText, Input, Label } from "reactstrap";
+import { Button, Container, Form, FormGroup, FormText, Input, Label, FormFeedback } from "reactstrap";
 
-const Reimbursement = () => {
-	return (
-	<Container>
-		<Form>
-			<FormGroup>
-				<Label for="eventSelect">Event:</Label>
-				<Input type="select" name="eventSelect" id="eventSelect">
-					<option>Test</option>
-				</Input>
-			</FormGroup>
-			<FormGroup>
-				<Label for="eventDescription">Description:</Label>
-				<Input type="textarea" name="eventDescription" id="eventDescription" />
-			</FormGroup>
-			<FormGroup>
-				<Label for="payTo">Pay To:</Label>
-				<Input type="text" name="payTo" id="payTo" />
-			</FormGroup>
-			<FormGroup>
-				<Label for="amount">Amount:</Label>
-				<Input type="text" name="amount" id="amount" />
-			</FormGroup>
-			<FormGroup>
-				<Label for="uploadReceipt">Upload Receipt:</Label>
-				<Input type="file" name="uploadReceipt" id="uploadReceipt" />
-				<FormText color="muted">
-					Please upload a digital copy of the reciept in one of the following formats: .png, .jpg, .jpeg, .pdf 
-				</FormText>
-			</FormGroup>
-			<FormGroup tag="fieldset">
-				<legend>Delivery Format:</legend>
-				<FormGroup check>
-					<Label check>
-						<Input type="radio" name="mail" />{' '}
-						Mail
-					</Label>
-				</FormGroup>
-				<FormGroup check>
-					<Label check>
-						<Input type="radio" name="sofs" />{' '}
-						Pick up at SOFS Office
-					</Label>
-				</FormGroup>
-			</FormGroup>
-			<Button>Submit</Button>
-		</Form>
-	</Container>
-	);
-};
+export default class ReimbursementForm extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			event: '',
+			description: 'test',
+			payTo: '',
+			amount: '',
+			file: '',
+			mail: false,
+			sofs: false,
+			eventValid: false,
+			descriptionValid: false,
+			payToValid: false,
+			amountValid: false,
+			fileValid: false,
+			deliveryFormatValid: false
+		}
 
-export default Reimbursement;
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		let field = '';
+		switch (event.target.id) {
+			case 'eventSelect':
+				field = 'event';
+				break;
+			case 'eventDescription':
+				field = 'description';
+				break;
+			case 'payTo':
+				field = 'payTo';
+				break;
+			case 'amount':
+				field = 'amount';
+				break;
+			case 'uploadReceipt':
+				field = 'file';
+				break;
+			case 'mail':
+				field = 'mail';
+				this.setState({sofs: 'off'});
+				break;
+			case 'sofs':
+				field = 'sofs';
+				this.setState({mail: 'off'});
+				break;
+			default:
+				break;
+		}
+		console.log(event.target.id + ": " + event.target.value);
+		this.setState({[field]: event.target.value});
+	}
+
+	handleSubmit(event) {
+		this.validateInput();
+		console.log(this.state);
+	}
+
+	isNotEmpty(val) {
+		return val != null && val != undefined && val != '';
+	}
+
+	isValidFileFormat(fp) {
+		return fp.endsWith('.pdf') || fp.endsWith('.png') || fp.endsWith('.jpg') || fp.endsWith('.jpeg');
+	}
+
+	validateInput() {
+		this.state.eventValid = this.isNotEmpty(this.state.event);
+		this.state.descriptionValid = this.isNotEmpty(this.state.description);
+		this.state.payToValid = this.isNotEmpty(this.state.payTo);
+		this.state.amountValid = this.isNotEmpty(this.state.amount);
+		this.state.fileValid = this.isNotEmpty(this.state.file) && this.isValidFileFormat(this.state.file);
+		this.state.deliveryFormatValid = (this.state.mail == 'on' || this.state.sofs == 'on') && !(this.state.mail == 'on' && this.state.sofs == 'on');
+	}
+
+	render() {
+		return (
+			<Container>
+				<Form>
+					<FormGroup>
+						<Label for="eventSelect">Event:</Label>
+						<Input type="select" name="eventSelect" id="eventSelect" value={this.state.event} onChange={this.handleChange}>
+							<option>Test</option>
+						</Input>
+					</FormGroup>
+					<FormGroup>
+						<Label for="eventDescription">Description:</Label>
+						<Input type="textarea" name="eventDescription" id="eventDescription" value={this.state.description} onChange={this.handleChange}/>
+					</FormGroup>
+					<FormGroup>
+						<Label for="payTo">Pay To:</Label>
+						<Input type="text" name="payTo" id="payTo" value={this.state.payTo} onChange={this.handleChange}/>
+					</FormGroup>
+					<FormGroup>
+						<Label for="amount">Amount:</Label>
+						<Input type="text" name="amount" id="amount" value={this.state.amount} onChange={this.handleChange}/>
+					</FormGroup>
+					<FormGroup>
+						<Label for="uploadReceipt">Upload Receipt:</Label>
+						<Input type="file" name="uploadReceipt" id="uploadReceipt" value={this.state.file} onChange={this.handleChange}/>
+						<FormText color="muted">
+							Please upload a digital copy of the reciept in one of the following formats: .png, .jpg, .jpeg, .pdf 
+						</FormText>
+					</FormGroup>
+					<FormGroup tag="fieldset">
+						<legend>Delivery Format:</legend>
+						<FormGroup check>
+							<Label check>
+								<Input type="radio" name="radio1" id="mail" onChange={this.handleChange}/>{' '}
+								Mail
+							</Label>
+						</FormGroup>
+						<FormGroup check>
+							<Label check>
+								<Input type="radio" name="radio1" id="sofs" onChange={this.handleChange}/>{' '}
+								Pick up at SOFS Office
+							</Label>
+						</FormGroup>
+					</FormGroup>
+					<Button onClick={this.handleSubmit}>Submit</Button>
+				</Form>
+			</Container>
+			);
+	}
+}
