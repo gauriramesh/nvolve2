3fa59036efe2b0437019f928eb56fe2b81a91a4f
Karl Shaffer <skmshaffer@gmail.com>
2019-10-22 16:12:31 -0500

    Finishes Unsaved Modal.

---
+++ src/components/UnsavedProgress.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./UnsavedProgress.css";
+
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+import { Redirect } from "react-router-dom";
+
+const UnsavedProgress = props => {
+  const [open, setOpen] = useState(true);
+  const [redirect, setRedirect] = useState(false);
+
+  return (
+    <Modal isOpen={open}>
+      <ModalHeader>Are you sure you want to leave?</ModalHeader>
+      <ModalBody>
+        Hey there!
+        <br />
+        <br />
+        It looks like you are working on a form and haven't yet saved your
+        changes. Are you sure you want to leave this page? The information
+        entered into the form will be lost.
+      </ModalBody>
+      <ModalFooter>
+        <Button color="secondary" onClick={() => setRedirect(true)}>
+          Leave
+        </Button>
+        <Button color="danger" onClick={() => setOpen(false)}>
+          Stay Here
+        </Button>
+      </ModalFooter>
+      {redirect && <Redirect to={props.destinationIfConfirmed} />}
+    </Modal>
+  );
+};
+
+export default UnsavedProgress;
