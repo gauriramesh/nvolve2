02518aafdbe8dae98b7952c1e22800d2ea18e328
gauri <gauri.ramesh15@gmail.com>
2019-10-25 15:05:39 -0500

    replaced hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Dashboard from "./pages/Dashboard";
 import Organization from "./pages/Organization";
